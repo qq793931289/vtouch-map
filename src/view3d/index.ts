501566dbc6275f30cdae6aa55c918a3677d6a9bf
bobby <793931289@qq.com>
2020-06-08 12:55:36 +0800

    fix typesctript build bug

--- src/view3d/index.ts
+++ src/view3d/index.ts
@@ -1,6 +1,7 @@
-export * from './camera';
+export * from './camera/camera';
+export * from './camera/cameracontroller';
 export * from './cartesian';
-export * from './constant';
+export * from './constant/config';
 export * from './interface';
 export * from './sprite';
 // export * from './viewer';
